d4ba9076924a980ae1ec0c2a3ec879744922fc09
GHOST <ghostdevbusiness@gmail.com>
2021-06-28 01:27:25 +0100

    fix: test

--- tests/fs/fs.test.js
+++ tests/fs/fs.test.js
@@ -6,7 +6,7 @@ import { readdirRecursive } from '../../dist/index.js';
 test('fn works', () => {
     const files = readdirRecursive('tests/fs/testFiles');
 
-    if (!files.every((p) => p.endsWith(test.txt)) || files.length != 2)
+    if (!files.every((p) => p.endsWith('test.txt')) || files.length != 2)
         throw new Error('Something went wrong');
 });
 
